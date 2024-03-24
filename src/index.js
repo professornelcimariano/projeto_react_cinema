9d93d1ca9a214df4ee433f3e33e400480475fc07
Professor Nelci Mariano <professornelcimariano@gmail.com>
2024-03-24 17:53:30 -0300

    Alterações no conteúdo

--- src/index.js
+++ src/index.js
@@ -1,19 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import AppRoutes from 'routes.js';
+import AppRoutes from './routes.js';
 import './index.css';
-{/*import App from './App';*/}
-{/*import reportWebVitals from './reportWebVitals';*/}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <AppRoutes />
-    {/*<App />*/} {/* Componente do React */}
-    {/* <h1>
-      Olá Mundo
-    </h1> */}
   </React.StrictMode>
 );
 
-//reportWebVitals();
